212b60bdf4c7215b78b10571788b3226beee3205
Vahid Moghaddam <v.moghaddam59@gmail.com>
2025-04-26 19:35:48 +0330

    sdfsdfsd

---
+++ FileManager/Global.asax.cs
@@ -0,0 +1,49 @@
+﻿using GleamTech.FileUltimate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace FileManager
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            FileUltimateConfiguration.Current.LicenseKey = "82Z56QJL2M-GDGUSF5D6R-NES2KS1HMK-8D421CMJSG-APT27QXK4F-U3A9FA6KEX-MWER28Q";
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
