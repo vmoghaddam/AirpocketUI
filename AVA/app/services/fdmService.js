a899b9efc759548306a7356ad05df59164a3c186
Vahid Moghaddam <v.moghaddam59@gmail.com>
2025-05-11 19:28:23 +0330

    fgffgfgf

---
+++ AVA/app/services/fdmService.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+app.factory('fdmService', ['$http', '$q', 'ngAuthSettings', '$rootScope', function ($http, $q, ngAuthSettings, $rootScope) {
+
+    var serviceFactory = {};
+
+    var _get_fmd_all = function (dt1, dt2) {
+        var deferred = $q.defer();
+        $http.get(api_fdm + "api/fdm/V2/events/" + dt1 + "/" + dt2).then(function (response) {
+            deferred.resolve(response.data);
+        }, function (err, status) {
+            // deferred.reject(Exeptions.getMessage(err));
+        });
+
+        return deferred.promise;
+    }
+    serviceFactory.get_fmd_all = _get_fmd_all;
+
+
+
+    return serviceFactory;
+
+}]);
