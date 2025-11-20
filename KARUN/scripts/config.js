afd7f6dd9532f42ff319a616e85ebd2c793fed1c
shahraeinisepehr <shahraeinisepehr@gmail.com>
2025-11-20 14:00:26 +0330

    KRN project update.

--- KARUN/scripts/config.js
+++ KARUN/scripts/config.js
@@ -42,7 +42,7 @@ Config.LocalData = {};
 /////////////////////////////////
 Config.Types = [
     { type: 'airport', table: 'ViewAirport' },
-   // { type: 'aidnc', table: 'ViewAid' },
+    // { type: 'aidnc', table: 'ViewAid' },
 
 ];
 Config.Fields = [
@@ -57,10 +57,10 @@ Config.Fields = [
 
 Config.MenuItems = [
     { moduleId: 2, key: 'library_book', title: 'Books & Documents', url: '/library/83/-1/-1', icon: 'content/images/booksg.png' },
-	 { moduleId: 2, key: 'library_document', title: 'PIFs', url: '/document', icon: 'content/images/docs2.png' },
+    { moduleId: 2, key: 'library_document', title: 'PIFs', url: '/document', icon: 'content/images/docs2.png' },
     { moduleId: 2, key: 'library_video', title: 'Videos', url: '/library/85/-1/-1', icon: 'content/images/Videos2.png' },
-   // { moduleId: 2, key: 'library_paper', title: 'Papers', url: '/library/84/-1/-1', icon: 'content/images/Papers2.png' },
-   
+    // { moduleId: 2, key: 'library_paper', title: 'Papers', url: '/library/84/-1/-1', icon: 'content/images/Papers2.png' },
+
     { moduleId: 2, key: 'library_people', title: 'People', url: '/person/book', icon: 'content/images/study2.png' },
 
     { moduleId: 2, key: 'library_notification', title: 'Notifications', url: '/notification', icon: 'content/images/notification2.png' },
@@ -70,122 +70,121 @@ Config.MenuItems = [
     //{ moduleId: 2, key: 'library_conference', title: 'Conferences', url: '/conference', icon: 'content/images/teamwork2.png' },
 
 
-    //{ moduleId: 1, key: 'profile_users', title: 'Users', url: '/users', icon: 'content/images/shield.png' },
-    { moduleId: 1, key: 'profile_person', title: 'Profiles', url: '/person', icon: 'content/images/group2.png' },
-    { moduleId: 1, key: 'profile_course_person', title: 'Courses', url: '/course/person', icon: 'content/images/course2.png' },
+    { moduleId: 1, key: 'profile_users', title: 'Users', url: '/users', icon: 'content/images/shield.png' },
+    { moduleId: 1, key: 'profile_dashboard', title: 'Dashboard', url: '/trn/dashboard', icon: 'content/images/trndashboard.png' },
     { moduleId: 1, key: 'profile_schedules', title: 'Schedules', url: '/trn/schedule/coursetype', icon: 'content/images/trnschedules.png' },
-	   { moduleId: 1, key: 'profile_dashboard', title: 'Dashboard', url: '/trn/dashboard', icon: 'content/images/trndashboard.png' },
-    { moduleId: 1, key: 'profile_person_certificate', title: 'History', url: '/trn/stat', icon: 'content/images/trnstat.png' },
-   
+    { moduleId: 1, key: 'profile_person', title: 'Profiles', url: '/person', icon: 'content/images/group2.png' },
     { moduleId: 1, key: 'profile_course_type', title: 'Course Type', url: '/course/type', icon: 'content/images/types2.png' },
-    { moduleId: 1, key: 'profile_course_person', title: 'Instructors', url: '/teachers', icon: 'content/images/teacher.png' },
-   
-	
-   // { moduleId: 1, key: 'profile_person_certificate', title: 'Training Profile', url: '/person/certificate', icon: 'content/images/trnprofile.png' },
-	// { moduleId: 1, key: 'profile_person_certificate', title: 'Training Profile (Groups)', url: '/groups/expiring', icon: 'content/images/trnprofile.png' },
-	//{ moduleId: 1, key: 'profile_person_certificate', title: 'Certificates', url: '/expiring/coursetype', icon: 'content/images/certificates2.png' },
-	
-	
-	
+    { moduleId: 1, key: 'profile_course_person', title: 'Courses', url: '/course/person', icon: 'content/images/course2.png' },
+    { moduleId: 1, key: 'profile_person_certificate', title: 'Training History', url: '/trn/stat', icon: 'content/images/trnstat.png' },
+    { moduleId: 1, key: 'profile_person_certificate', title: 'Training Profile', url: '/person/certificate', icon: 'content/images/trnprofile.png' },
+    { moduleId: 1, key: 'profile_person_certificate', title: 'Training Profile (Groups)', url: '/groups/expiring', icon: 'content/images/trnprofile.png' },
+    { moduleId: 1, key: 'profile_person_certificate', title: 'Certificates', url: '/expiring/coursetype', icon: 'content/images/certificates2.png' },
+    { moduleId: 1, key: 'profile_course_person', title: 'Teachers', url: '/teachers', icon: 'content/images/teacher.png' },
+
+
     //{ moduleId: 1, key: 'profile_person_course', title: 'Employees Courses', url: '/person/course', icon: 'content/images/setting2.png' },
-   //{ moduleId: 1, key: 'profile_course', title: 'Archive', url: '/course', icon: 'content/images/cabinets2.png' },
-   
-     
-   
-   // { moduleId: 1, key: 'profile_location', title: 'Departments', url: '/location', icon: 'content/images/office2.png' },
-    
-    //{ moduleId: 1, key: 'profile_aircrafttype', title: 'Aircraft Types', url: '/aircrafttype', icon: 'content/images/actype2.png' },
-    
-   // { moduleId: 1, key: 'profile_educationfield', title: 'Education Fields', url: '/option/59', icon: 'content/images/fields2.png' },
-    
-    //{ moduleId: 1, key: 'profile_post', title: 'Posts', url: '/option/36', icon: 'content/images/diagram2.png' },
-  
-   // { moduleId: 1, key: 'profile_group', title: 'Groups', url: '/jobgroup', icon: 'content/images/circle2.png' },
+    //{ moduleId: 1, key: 'profile_course', title: 'Archive', url: '/course', icon: 'content/images/cabinets2.png' },
 
-   // { moduleId: 1, key: 'profile_airports', title: 'Airports', url: '/airport', icon: 'content/images/airport.png' },
 
-{ moduleId: 3, key: 'profile_person', title: 'Employees', url: '/person', icon: 'content/images/group2.png' },
+
+    { moduleId: 1, key: 'profile_location', title: 'Departments', url: '/location', icon: 'content/images/office2.png' },
+
+    { moduleId: 1, key: 'profile_aircrafttype', title: 'Aircraft Types', url: '/aircrafttype', icon: 'content/images/actype2.png' },
+
+    { moduleId: 1, key: 'profile_educationfield', title: 'Education Fields', url: '/option/59', icon: 'content/images/fields2.png' },
+
+    { moduleId: 1, key: 'profile_post', title: 'Posts', url: '/option/36', icon: 'content/images/diagram2.png' },
+
+    { moduleId: 1, key: 'profile_group', title: 'Groups', url: '/jobgroup', icon: 'content/images/circle2.png' },
+
+    { moduleId: 1, key: 'profile_airports', title: 'Airports', url: '/airport', icon: 'content/images/airport.png' },
+
+    { moduleId: 3, key: 'profile_person', title: 'Employees', url: '/person', icon: 'content/images/group2.png' },
     { moduleId: 3, key: 'flight_planning', title: 'Planning', url: '/commercial/planning', icon: 'content/images/actype2.png' },
-     { moduleId: 3, key: 'flight_planning_board', title: 'Planning Board', url: '/plan/board', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_planning_board', title: 'Planning Board', url: '/plan/board', icon: 'content/images/actype2.png' },
 
-     { moduleId: 3, key: 'flight_plans', title: 'Plans', url: '/commercial/plans', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_plans', title: 'Plans', url: '/commercial/plans', icon: 'content/images/actype2.png' },
 
     { moduleId: 3, key: 'flight_assign_register', title: 'Registers', url: '/maintenance/flights/calendar', icon: 'content/images/actype2.png' },
-     { moduleId: 3, key: 'flight_change_register', title: 'Registers', url: '/maintenance/flights/registers', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_change_register', title: 'Registers', url: '/maintenance/flights/registers', icon: 'content/images/actype2.png' },
     { moduleId: 3, key: 'flight_assign_crew_cockpit', title: 'FDPs (Cockpit)', url: '/crewtest', icon: 'content/images/actype2.png' },
     { moduleId: 3, key: 'flight_assign_crew_cabin', title: 'FDPs (Cabin)', url: '/crewtest', icon: 'content/images/actype2.png' },
 
-     { moduleId: 3, key: 'flight_assign_crew_cockpit', title: 'FDPs (Cockpit)', url: '/crewtest', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_assign_crew_cockpit', title: 'FDPs (Cockpit)', url: '/crewtest', icon: 'content/images/actype2.png' },
     { moduleId: 3, key: 'flight_assign_crew_cabin', title: 'FDPs (Cabin)', url: '/crewtest', icon: 'content/images/actype2.png' },
-    
-	 { moduleId: 3, key: 'roster', title: 'Crew Scheduler', url: '/roster', icon: 'content/images/actype2.png' },
-	 { moduleId: 3, key: 'scheduling', title: 'Crew Scheduler (New)', url: '/scheduling', icon: 'content/images/nsch.png' },
-	  { moduleId: 3, key: 'dutytimeline', title: 'Duties Timeline', url: '/duty/timeline', icon: 'content/images/dtt.png' },
-	  { moduleId: 3, key: 'timeline', title: 'TimeLine', url: '/duty/timeline/z', icon: 'content/images/actype2.png' },
-	   
-{ moduleId: 3, key: 'flight_calendar_cockpit', title: 'Calendar (Cockpit)', url: '/crewtest', icon: 'content/images/actype2.png' },
-{ moduleId: 3, key: 'flight_calendar_cabin', title: 'Calendar (Cabin)', url: '/crewtest', icon: 'content/images/actype2.png' },
+
+    { moduleId: 3, key: 'roster', title: 'Crew Scheduler', url: '/roster', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'scheduling', title: 'Crew Scheduler (New)', url: '/scheduling', icon: 'content/images/nsch.png' },
+    { moduleId: 3, key: 'dutytimeline', title: 'Duties Timeline', url: '/duty/timeline', icon: 'content/images/dtt.png' },
+    { moduleId: 3, key: 'timeline', title: 'TimeLine', url: '/duty/timeline/z', icon: 'content/images/actype2.png' },
+
+    { moduleId: 3, key: 'flight_calendar_cockpit', title: 'Calendar (Cockpit)', url: '/crewtest', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_calendar_cabin', title: 'Calendar (Cabin)', url: '/crewtest', icon: 'content/images/actype2.png' },
 
 
     { moduleId: 3, key: 'flight_fuel', title: 'Fuel', url: '/fuel/report', icon: 'content/images/fuel.png' },
     { moduleId: 3, key: 'flight_crew', title: 'Crew', url: '/crew/report', icon: 'content/images/actype2.png' },
 
-     { moduleId: 3, key: 'flight_crew_time', title: 'Crew Flight Time', url: '/crew/time/report', icon: 'content/images/actype2.png' },
-	  { moduleId: 3, key: 'flight_crew_fixtime', title: 'Crew Fix Time', url: '/crew/report/fixtime', icon: 'content/images/actype2.png' },
-	   { moduleId: 3, key: 'fixedtime_edit', title: 'Crew Fix Time', url: '/fixedtime', icon: 'content/images/actype2.png' },
-	   { moduleId: 3, key: 'register_flights_report', title: 'Register Fix Time', url: '/reg/flights/monthly', icon: 'content/images/actype2.png' },
-	   
-	   { moduleId: 3, key: 'summary_flights_report', title: 'Register Fix Time', url: '/flights/monthly', icon: 'content/images/actype2.png' },
-	   
-      { moduleId: 3, key: 'flight_roster', title: 'Daily Roster', url: '/roster', icon: 'content/images/actype2.png' },
-       { moduleId: 3, key: 'flight_report', title: 'Flights', url: '/flights/report', icon: 'content/images/actype2.png' },
-	  { moduleId: 3, key: 'flight_daily', title: 'Daily Report', url: '/report/flight/daily', icon: 'content/images/fltdaily.png' },
+    { moduleId: 3, key: 'flight_crew_time', title: 'Crew Flight Time', url: '/crew/time/report', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_crew_fixtime', title: 'Crew Fix Time', url: '/crew/report/fixtime', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'fixedtime_edit', title: 'Crew Fix Time', url: '/fixedtime', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'register_flights_report', title: 'Register Fix Time', url: '/reg/flights/monthly', icon: 'content/images/actype2.png' },
+
+    { moduleId: 3, key: 'summary_flights_report', title: 'Register Fix Time', url: '/flights/monthly', icon: 'content/images/actype2.png' },
+
+    { moduleId: 3, key: 'flight_roster', title: 'Daily Roster', url: '/roster', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_report', title: 'Flights', url: '/flights/report', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_daily', title: 'Daily Report', url: '/report/flight/daily', icon: 'content/images/fltdaily.png' },
+    { moduleId: 3, key: 'flight_pax', title: 'Pax Report', url: '/report/flight/pax', icon: 'content/images/passengers.png' },
 
     { moduleId: 3, key: 'flight_board', title: 'Flight Board 1', url: '/flight/board', icon: 'content/images/actype2.png' },
-     { moduleId: 3, key: 'flight_board_ceo', title: 'Flight Board ceo', url: '/board/ceo', icon: 'content/images/actype2.png' },
-     { moduleId: 3, key: 'flight_aog', title: 'Flight AOG 1', url: '/reg/availability', icon: 'content/images/actype2.png' },
-      { moduleId: 3, key: 'flight_blocktime', title: 'Flight block Times', url: '/route', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_board_ceo', title: 'Flight Board ceo', url: '/board/ceo', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_aog', title: 'Flight AOG 1', url: '/reg/availability', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'flight_blocktime', title: 'Flight block Times', url: '/route', icon: 'content/images/actype2.png' },
     { moduleId: 3, key: 'flight_board_2', title: 'Flight Board 2', url: '/dispatch/flights/THR/0', icon: 'content/images/actype2.png' },
-     
+
     { moduleId: 3, key: 'flight_airports', title: 'Airports', url: '/dispatch/airport', icon: 'content/images/airport.png' },
     { moduleId: 3, key: 'flight_archive', title: 'Flights Archive', url: '/flight/archive', icon: 'content/images/actype2.png' },
     { moduleId: 3, key: 'flight_delay', title: 'Delay', url: '/delay/report', icon: 'content/images/delay.png' },
 
     { moduleId: 3, key: 'flight_dc', title: 'Delay', url: '/delay/codes', icon: 'content/images/delay.png' },
-	
-	
-	   { moduleId: 3, key: 'fdr_report', title: 'FDR Report', url: '/fdr', icon: 'content/images/actype2.png' },
-      { moduleId: 3, key: 'fin_report', title: 'Fin. Daily Report', url: '/fin/report', icon: 'content/images/actype2.png' },
-       { moduleId: 3, key: 'fin_monthly_report', title: 'Fin. Monthly Report', url: '/fin/monthly/report', icon: 'content/images/actype2.png' },
 
- { moduleId: 3, key: 'delays_report', title: 'Delays Report', url: '/delays', icon: 'content/images/actype2.png' },
- 
-     { moduleId: 3, key: 'atr_forma', title: 'Form A', url: '/forma', icon: 'content/images/forma.png' },
-	  { moduleId: 3, key: 'atr_formmovaled', title: 'Form A', url: '/formmovaled', icon: 'content/images/forma.png' },
-      { moduleId: 3, key: 'atr_citypair', title: 'City-Pair Report', url: '/citypair', icon: 'content/images/citypair.png' },
-	   { moduleId: 3, key: 'efb', title: 'EFB Report', url: '/flights/efbs', icon: 'content/images/efb.png' },
- 	{ moduleId: 3, key: 'trn_roster', title: 'Roster', url: '/sim', icon: 'content/images/delaybi.png' },
+
+    { moduleId: 3, key: 'fdr_report', title: 'FDR Report', url: '/fdr', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'fin_report', title: 'Fin. Daily Report', url: '/fin/report', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'fin_monthly_report', title: 'Fin. Monthly Report', url: '/fin/monthly/report', icon: 'content/images/actype2.png' },
+
+    { moduleId: 3, key: 'delays_report', title: 'Delays Report', url: '/delays', icon: 'content/images/actype2.png' },
+    { moduleId: 3, key: 'delays_report_flight', title: 'Delayed Flights', url: '/delays/flight', icon: 'content/images/actype2.png' },
+
+    { moduleId: 3, key: 'atr_forma', title: 'Form A', url: '/forma', icon: 'content/images/forma.png' },
+    { moduleId: 3, key: 'atr_formmovaled', title: 'Form A', url: '/formmovaled', icon: 'content/images/forma.png' },
+    { moduleId: 3, key: 'atr_citypair', title: 'City-Pair Report', url: '/citypair', icon: 'content/images/citypair.png' },
+    { moduleId: 3, key: 'efb', title: 'EFB Report', url: '/flights/efbs', icon: 'content/images/efb.png' },
+    { moduleId: 3, key: 'user_manager', title: 'Users', url: '/users', icon: 'content/images/user.png' },
+
 ];
 
 ///////////////////////////////
 Exceptions = {};
 Exceptions.getMessage = function (error) {
 
-    
+
 
     if (error.data.ModelState) {
 
         if (error.data.ModelState.errs) {
 
             //console.log(error.data.ModelState.errs);
-             
+
             return {
                 message: error.data.ModelState.errs
             };
             //return
-           // {
-           //     message: "XXXXX" //error.data.ModelState.errs.j
-           // };
+            // {
+            //     message: "XXXXX" //error.data.ModelState.errs.j
+            // };
         }
         else
             return {
@@ -193,7 +192,7 @@ Exceptions.getMessage = function (error) {
             };
     }
     if (error.data.indexOf('Location-04') != -1) {
-        
+
         return {
             message: "Assigned employees found.The selected item cannot be deleted."
         };
@@ -214,12 +213,12 @@ Exceptions.getMessage = function (error) {
     if (error.data.indexOf('Option-05:Course found') != -1) {
         return { message: "Assigned courses found.The selected item cannot be deleted." };
     }
-    return { message:error.status+' '+ error.statusText+' '+error.data };
+    return { message: error.status + ' ' + error.statusText + ' ' + error.data };
 };
 /////////////////////////////
 Colors = {};
 Colors.Palette = [
-    
+
     '#ff275d',
     '#00b0f0',
     '#2cb77b',
@@ -251,7 +250,7 @@ Colors.Palette = [
     '#f6765a',
     '#9f765a',
     '#9f76ab',
-    
+
 ];
 Colors.getRandom = function () {
     var color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
@@ -261,11 +260,11 @@ Colors.getRandom = function () {
 Colors.getColor = function (index) {
     if (index <= Colors.Palette.length - 1)
         return Colors.Palette[index];
-    return Colors.getRandom ();
+    return Colors.getRandom();
 };
 Colors.getColorReverse = function (index) {
     if (index <= Colors.Palette.length - 1)
-        return Colors.Palette[Colors.Palette.length - 1-index];
+        return Colors.Palette[Colors.Palette.length - 1 - index];
     return Colors.getRandom();
 };
 
