07b9d523deaf630b84f7713aff14bc8524d3cbc0
Vahid Moghaddam <v.moghaddam59@gmail.com>
2025-12-20 10:13:45 +0330

    یبلیبلیبل

--- VRH/app/services/cmsService.js
+++ VRH/app/services/cmsService.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-app.factory('qahService', ['$http', '$q', 'ngAuthSettings', '$rootScope', function ($http, $q, ngAuthSettings, $rootScope) {
+app.factory('cmsService', ['$http', '$q', 'ngAuthSettings', '$rootScope', function ($http, $q, ngAuthSettings, $rootScope) {
 
     var api_cms = 'http://localhost:9070/';
     var serviceFactory = {};
@@ -7,9 +7,9 @@ app.factory('qahService', ['$http', '$q', 'ngAuthSettings', '$rootScope', functi
 
     var token = "-";
 
-    var _get_reports = function (df,dt,type,register) {
+    var _get_reports = function (df,dt,type,register,flight_id) {
         var deferred = $q.defer();
-        $http.get(api_cms + 'api/cms/reports/'+token+'?df='+df+'&dt='+dt+'type='+type+'&register='+register).then(function (response) {
+        $http.get(api_cms + 'api/cms/reports/'+token+'?df='+df+'&dt='+dt+'&type='+type+'&register='+register+'&flight_id='+flight_id).then(function (response) {
             deferred.resolve(response.data);
         }, function (err, status) {
             deferred.reject(Exceptions.getMessage(err));
